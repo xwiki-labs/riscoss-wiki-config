95365f235e903a6d924603494770288e23a1cd04
Caleb James DeLisle <cjd@cjdns.fr>
2015-03-13 16:44:10 +0100

    added configuration repo

---
+++ readme.md
@@ -0,0 +1,4 @@
+# RISCOSS Platform Domain Manager Configuration
+
+This repository contains default configuration for the RISCOSS Platform Domain Manager.
+Please see https://github.com/RISCOSS/riscoss-platform-build for a starting point.
---
+++ src/xwiki/this.js
@@ -0,0 +1,6 @@
+Package(function (pack, XWiki) {
+    pack.setName("RISCOSS Domain Manager Configuration");
+    pack.setDescription("RISCOSS Platform - Domain Manager - Configuration");
+    pack.setExtensionId("eu.riscoss:riscoss-platform-dm-config");
+    pack.setXarName("riscoss-platform-dm-config-0.24-SNAPSHOT.xar");
+});
