3fc51e8a5093724f9df99407a0f6d61d204aff5a
mouhb <mohamed.boussa@xwiki.com>
2015-04-03 10:10:10 +0100

    Add the Riscoss configuration document

---
+++ src/xwiki/RISCOSSPlatformCode/RISCOSSConfiguration/this.js
@@ -0,0 +1,14 @@
+XWikiDoc(function (doc) {
+    doc.setParent("RISCOSSPlatformCode.WebHome");
+    doc.setAuthor("xwiki:XWiki.superadmin");
+    doc.setCustomClass(undefined);
+    doc.setContentAuthor("xwiki:XWiki.superadmin");
+    doc.setCreationDate("1427903671000");
+    doc.setDate("1428051887000");
+    doc.setContentUpdateDate("1427884216000");
+    doc.setVersion("1.7");
+    doc.setDefaultTemplate(undefined);
+    doc.setValidationScript(undefined);
+    doc.setComment("Imported from XAR");
+    doc.setMinorEdit("true");
+});
